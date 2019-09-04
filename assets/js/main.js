cebf9553b652406c631645b4cc8459cc6925a586
mchightower <53190622+mchightower@users.noreply.github.com>
2019-09-04 11:38:09 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,7 @@
 		xsmall: '(max-width: 480px)',
 		xxsmall: '(max-width: 360px)'
 	});
-		//Track Events Test
-		pendo.track("Registered", {
-  			plan: "Pro Annual",
- 			accountType: "Facebook"
-					});	
+
 	$(function() {
 
 		var	$window = $(window),
